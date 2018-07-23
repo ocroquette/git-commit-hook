9d10d913a3a4f6dd1ed1256c8588ef1bd805817e
Olivier Croquette <ocroquette@free.fr>
2018-07-23 21:43:46 +0200

    Support indentation in word wrap

--- src/main/java/com/github/ocroquette/commithook/CommitMessageUpdaterWordWrap.java
+++ src/main/java/com/github/ocroquette/commithook/CommitMessageUpdaterWordWrap.java
@@ -1,6 +1,8 @@
 package com.github.ocroquette.commithook;
 
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Wraps the text automatically
@@ -21,17 +23,34 @@ public class CommitMessageUpdaterWordWrap implements CommitMessageUpdater {
 
     private static final String LINK_PREFIX = "[a-z]://.*";
 
+    private String indentString = "";
+
     public void update(CommitMessage commitMessage) {
         List<String> textLines = commitMessage.getTextLines();
         checkFirstLines(commitMessage);
+        final Pattern spaces = Pattern.compile("(\\s*).*");
+        boolean isOriginalLine = true;
         for(int lineIndex = 1 ; lineIndex < textLines.size() ; lineIndex++) {
             String line = textLines.get(lineIndex);
+            if (isOriginalLine) {
+                Matcher matcher = spaces.matcher(line);
+                matcher.matches();
+                if(indentString.length() != line.length())
+                    indentString = matcher.group(1);
+            }
+            isOriginalLine = true;
             if (line.length()> MAX_LENGTH) {
-                int splitIndex = getPotentialSplitIndex(line);
+                int splitIndex = getPotentialSplitIndex(line, indentString.length());
                 if (splitIndex != -1) {
                     textLines.remove(lineIndex);
                     textLines.add(lineIndex, line.substring(0, splitIndex + 1 ));
-                    textLines.add(lineIndex + 1, line.substring(splitIndex + 1, line.length()));
+
+                    String nextLine = line.substring(splitIndex + 1, line.length());
+                    nextLine.replaceAll("^\\s+", "");
+                    nextLine = indentString + nextLine;
+                    textLines.add(lineIndex + 1, nextLine);
+
+                    isOriginalLine = false;
                 }
             }
         }
@@ -49,10 +68,8 @@ public class CommitMessageUpdaterWordWrap implements CommitMessageUpdater {
             System.err.println("WARNING: second line of commit message is not empty as it should be.");
     }
 
-    private int getPotentialSplitIndex(String s) {
+    private int getPotentialSplitIndex(String s, int indentLength) {
         List<Integer> breakPoints = new ArrayList<>();
-
-        System.out.println("getPotentialSplitIndex " + s);
 
         boolean inLink = false;
         for(int i = 1; i < s.length() ; i++) {
@@ -73,7 +90,7 @@ public class CommitMessageUpdaterWordWrap implements CommitMessageUpdater {
 
         // Try to split so that the first line is short enough
         for (int index = breakPoints.size()-1 ; index > 0 ; index--)
-            if(breakPoints.get(index)  < MAX_LENGTH)
+            if(breakPoints.get(index)  < MAX_LENGTH - indentLength)
                 return breakPoints.get(index);
 
         // Plan B: try to have first line as short as possible
--- src/test/groovy/com/github/ocroquette/commithook/CommitMessageUpdaterWordWrapTest.groovy
+++ src/test/groovy/com/github/ocroquette/commithook/CommitMessageUpdaterWordWrapTest.groovy
@@ -62,6 +62,37 @@ Change-Id: Iee5559fbee98decbf54b03509e72a6fc1a0d24e6
 """)
     }
 
+    def "Word wrap commit-message-with-indent.txt"() {
+        given:
+        String input = this.getClass().getResource( '/commit-message-with-indent.txt' ).text
+        CommitMessage cm = new CommitMessage(input)
+        when:
+        createInstance().update(cm)
+        def lines = cm.getTextLines()
+        then:
+        TestHelper.compareLines(lines,
+                """Introduced syntax error
+
+Blabla
+  Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean 
+  commodo ligula eget dolor. Aenean massa. Cum sociis natoque 
+  penatibus et magnis dis parturient montes, nascetur ridiculus mus. 
+  Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.
+Nulla consequat massa quis enim. Donec pede justo, fringilla vel, 
+aliquet nec.
+    Vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, 
+    venenatis vitae, justo. Nullam dictum felis eu pede mollis 
+    pretium. Integer tincidunt. Cras dapibus. Vivamus elementum 
+    semper nisi.
+
+Some more text
+
+    A even more
+
+Change-Id: Iee5559fbee98decbf54b03509e72a6fc1a0d24e6
+""")
+    }
+
     def createInstance() {
         return new CommitMessageUpdaterWordWrap();
     }
