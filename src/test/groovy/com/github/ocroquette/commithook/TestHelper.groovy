065679a5d5992be61e5243f48d0240e914fb1742
Olivier Croquette <ocroquette@free.fr>
2018-11-11 21:51:21 +0100

    Fixed unit tests on Windows (line endings)

--- src/test/groovy/com/github/ocroquette/commithook/TestHelper.groovy
+++ src/test/groovy/com/github/ocroquette/commithook/TestHelper.groovy
@@ -1,15 +1,14 @@
 package com.github.ocroquette.commithook
 
 class TestHelper {
-    static final String EOL = System.getProperty("line.separator");
-
     static void compareLines(List<String> realLines, String expectedLinesRaw) {
-        List<String> expectedLines = expectedLinesRaw.split(EOL)
+        // Replace Windows CRLF by LF (if any), and split on LF
+        List<String> expectedLines = expectedLinesRaw.replaceAll("\r\n", "\n").split("\n")
         int max = Math.max(realLines.size(), expectedLines.size());
         for ( int i = 0 ; i < realLines.size() ; i++ ) {
             String realLine = realLines.getAt(i)
             String expectedLine = expectedLines.getAt(i)
-            assert realLine == expectedLine : "Mismatch at line $i"
+            assert realLine == expectedLine : "Mismatch at line ${i+1}"
         }
     }
 }
